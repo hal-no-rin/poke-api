6ace2b227c689bf68bb18a04b16669ea1c3f9eda
ShibayamaMaaku <shibayama.maaku@gmail.com>
2024-04-28 11:32:09 +0900

    axiosでpokemon-apiを使用する

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const axios = require('axios')
+
+const getUser = async () => {
+    try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
+
+        console.log(response.data.name)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+getUser()
